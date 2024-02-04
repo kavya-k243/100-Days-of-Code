a78b25242f44117361b53e48cc6a882275340b02
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-02-04 19:30:40 +0530

    Day 11

---
+++ Day 11/4 Sum .md
@@ -0,0 +1,20 @@
+Problem Statement: Given an array of N integers, your task is to find unique quads that add up to give a target value. In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c], arr[d]] such that their sum is equal to a given target.
+
+Pre-req: 3-sum problem and 2-sum problem
+
+Note:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+arr[a] + arr[b] + arr[c] + arr[d] == target
+
+Examples
+Example 1:
+Input Format: arr[] = [1,0,-1,0,-2,2], target = 0
+Result: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Explanation: We have to find unique quadruplets from the array such that the sum of those elements is equal to the target sum given that is 0. The result obtained is such that the sum of the quadruplets yields 0.
+
+Example 2:
+Input Format: arr[] = [4,3,3,4,4,2,1,2,1,1], target = 9
+Result: [[1,1,3,4],[1,2,2,4],[1,2,3,3]]
+Explanation: The sum of all the quadruplets is equal to the target i.e. 9.
---
+++ Day 11/Solution.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size(); 
+    set<vector<int>> st;
+
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            set<long long> hashset;
+            for (int k = j + 1; k < n; k++) {
+               
+                long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long fourth = target - sum;
+                if (hashset.find(fourth) != hashset.end()) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], (int)(fourth)};
+                    sort(temp.begin(), temp.end());
+                    st.insert(temp);
+                }
+           
+                hashset.insert(nums[k]);
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> ans = fourSum(nums, target);
+    cout << "The quadruplets are: \n";
+    for (auto it : ans) {
+        cout << "[";
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
+

7001b60e6611f2dcf7c28a46c17b5c8a52284bab
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-02-11 01:02:08 +0530

    Day 18

---
+++ Day 18/Rotate Image by 90 degree.md
@@ -0,0 +1,20 @@
+Problem Statement: Given a matrix, your task is to rotate the matrix 90 degrees clockwise.
+
+Note: Rotate matrix 90 degrees anticlockwise
+
+Examples
+Example 1:
+
+Input: [[1,2,3],[4,5,6],[7,8,9]]
+
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix.
+
+Example 2:
+
+Input: [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+
+Output:[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+Explanation: Rotate the matrix simply by 90 degree clockwise and return the matrix
---
+++ Day 18/Solution.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+void rotate(vector < vector < int >> & matrix) {
+    int n = matrix.size();
+
+    for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
+    }
+    }
+    
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
+}
+
+int main() {
+    vector < vector < int >> arr;
+    arr =  {{11, 22, 33}, {14, 15, 16}, {37, 38, 59}};
+    rotate(arr);
+    cout << "Rotated Image" << endl;
+    for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+        cout << arr[i][j] << " ";
+    }
+    cout << "n";
+    }
+
+}

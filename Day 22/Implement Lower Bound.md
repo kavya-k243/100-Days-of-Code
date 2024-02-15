78d0facf420d1e3a8eb8791dc0ae1910960e6611
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-02-15 20:56:45 +0530

    Day 22

---
+++ Day 22/Implement Lower Bound.md
@@ -0,0 +1,14 @@
+Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the lower bound of x.
+
+Pre-requisite: Binary Search algorithm
+
+Examples
+Example 1:
+Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+Result: 1
+Explanation: Index 1 is the smallest index such that arr[1] >= x.
+
+Example 2:
+Input Format: N = 5, arr[] = {3,5,8,15,19}, x = 9
+Result: 3
+Explanation: Index 3 is the smallest index such that arr[3] >= x.
---
+++ Day 22/Solultion.cpp
@@ -0,0 +1,31 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+   
+        if (arr[mid] >= x) {
+            ans = mid;
+            
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; 
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}

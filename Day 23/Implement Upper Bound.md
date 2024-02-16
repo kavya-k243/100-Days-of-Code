78f24408a89186f51372da87a6d33bc3663c63b1
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-02-16 17:37:20 +0530

    Day 23

---
+++ Day 23/Implement Upper Bound.md
@@ -0,0 +1,14 @@
+Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the upper bound of x.
+
+Pre-requisite: Binary Search algorithm
+
+Examples
+Example 1:
+Input Format: N = 4, arr[] = {1,2,2,3}, x = 2
+Result: 3
+Explanation: Index 3 is the smallest index such that arr[3] > x.
+
+Example 2:
+Input Format: N = 6, arr[] = {3,5,8,9,15,19}, x = 9
+Result: 4
+Explanation: Index 4 is the smallest index such that arr[4] > x.
---
+++ Day 23/Solution.cpp
@@ -0,0 +1,31 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+   
+        if (arr[mid] > x) {
+            ans = mid;
+           
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; 
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}

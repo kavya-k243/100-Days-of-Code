a975cd2e1694591a66403d3ab0c9d5b1d44fd7aa
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-02-21 11:25:59 +0530

    Day 28

---
+++ Day 28/Kth Missing Positive Number.md
@@ -0,0 +1,11 @@
+Problem Statement: You are given a strictly increasing array ‘vec’ and a positive integer ‘k’. Find the ‘kth’ positive integer missing from ‘vec’.
+
+Examples
+Example 1:
+Input Format: vec[]={4,7,9,10}, k = 1
+Result: 1
+Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 1, the first missing element is 1.
+Example 2:
+Input Format: vec[]={4,7,9,10}, k = 4
+Result: 5
+Explanation: The missing numbers are 1, 2, 3, 5, 6, 8, 11, 12, ……, and so on. Since 'k' is 4, the fourth missing element is 5.
---
+++ Day 28/Solution.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingK(vector < int > vec, int n, int k) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int missing = vec[mid] - (mid + 1);
+        if (missing < k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return k + high + 1;
+}
+
+int main()
+{
+    vector<int> vec = {4, 7, 9, 10};
+    int n = 4, k = 4;
+    int ans = missingK(vec, n, k);
+    cout << "The missing number is: " << ans << "\n";
+    return 0;
+}

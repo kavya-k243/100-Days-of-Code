d9c503eb944a249aa35f305d1a1a98e03e348c26
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-03-02 20:16:32 +0530

    Day 38

---
+++ Day 38/Solution.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+string result(string s)
+{
+    int left = 0;
+    int right = s.length()-1;
+    
+    string temp="";
+    string ans="";
+    
+  
+    while (left <= right) {
+        char ch= s[left];
+        if (ch != ' ') {
+            temp += ch;
+        } else if (ch == ' ') {
+            if (ans!="") ans = temp + " " + ans;
+            else ans = temp;
+            temp = "";
+        }
+        left++;
+    }
+    
+   
+    if (temp!="") {
+        if (ans!="") ans = temp + " " + ans;
+        else ans = temp;
+    }
+    
+    return ans;    
+}
+int main()
+{
+    string st="My name is Kavya Khandelwal";
+    cout<<"Before reversing words: "<<endl;
+    cout<<st<<endl;
+    cout<<"After reversing words: "<<endl;
+    cout<<result(st);
+    return 0;
+}
---
+++ Day 38/a.md
@@ -0,0 +1,11 @@
+Problem Statement: Given a string s, reverse the words of the string.
+
+Examples:
+
+Example 1:
+Input: s=”this is an amazing program”
+Output: “program amazing an is this”
+
+Example 2:
+Input: s=”This is decent”
+Output: “decent is This”

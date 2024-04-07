ba6f13dbff14488548aba34ac41ce2d455c30cc3
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-04-07 18:08:07 +0530

    Day 73

---
+++ Day 73/Area of largest rectangle in Histogram.md
@@ -0,0 +1,1 @@
+Problem Statement: Given an array of integers heights representing the histogram's bar height where the width of each bar is 1  return the area of the largest rectangle in histogram.
---
+++ Day 73/Solution.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    int largestRectangleArea(vector < int > & heights) {
+      int n = heights.size();
+      stack < int > st;
+      int leftsmall[n], rightsmall[n];
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && heights[st.top()] >= heights[i]) {
+          st.pop();
+        }
+        if (st.empty())
+          leftsmall[i] = 0;
+        else
+          leftsmall[i] = st.top() + 1;
+        st.push(i);
+      }
+      while (!st.empty())
+        st.pop();
+
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && heights[st.top()] >= heights[i])
+          st.pop();
+
+        if (st.empty())
+          rightsmall[i] = n - 1;
+        else
+          rightsmall[i] = st.top() - 1;
+
+        st.push(i);
+      }
+      int maxA = 0;
+      for (int i = 0; i < n; i++) {
+        maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
+      }
+      return maxA;
+    }
+};
+int main() {
+  vector<int> heights = {2, 1, 5, 6, 2, 3, 1};
+  Solution obj;
+  cout << "The largest area in the histogram is " << obj.largestRectangleArea(heights); 
+  return 0;
+}

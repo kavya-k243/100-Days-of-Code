ec6fcb9642153fb66d10ec71d98105c0b58e3d8a
Parth2206 <89535516+kavya-k243@users.noreply.github.com>
2024-04-15 13:53:19 +0530

    Day 82

---
+++ Day 82/Merge Overlapping Sub-intervals.md
@@ -0,0 +1,1 @@
+Problem Statement: Given an array of intervals, merge all the overlapping intervals and return an array of non-overlapping intervals.
---
+++ Day 82/Solution.cpp
@@ -0,0 +1,35 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
+    int n = arr.size(); 
+    sort(arr.begin(), arr.end());
+
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++) {
+        if (ans.empty() || arr[i][0] > ans.back()[1]) {
+            ans.push_back(arr[i]);
+        }
+        else {
+            ans.back()[1] = max(ans.back()[1], arr[i][1]);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<vector<int>> arr = {{2, 2}, {1, 0}, {9, 26}, {11, 29}};
+    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
+    cout << "The merged intervals are: " << "\n";
+    for (auto it : ans) {
+        cout << "[" << it[0] << ", " << it[1] << "] ";
+    }
+    cout << endl;
+    return 0;
+}
+
